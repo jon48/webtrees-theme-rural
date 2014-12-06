afa22a0f63403e1a93b58c2172f76520daec9a41
Jonathan Jaubart <dev@jaubart.com>
2014-12-06 14:07:36 +0000

    RESOLVED #JJ181: Upgrade to webtrees 1.6.1 - Adapt Perso add-ons http://bugs.jaubart.com/view.php?id=181

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
 // Copyright (C) 2014 webtrees development team.
 //
 // Derived from PhpGedView
-// Copyright (C) 2002 to 2009  PGV Development Team.  All rights reserved.
+// Copyright (C) 2002 to 2009  PGV Development Team.
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -112,7 +112,7 @@ $this
 				<ul class="makeMenu">
 					<li>
 					<?php 
-					if (WT_USER_ID) {
+					if (Auth::check()) {
 						echo '<a href="edituser.php">', WT_I18N::translate('Logged in as '), ' ', WT_Filter::escapeHtml(Auth::user()->getRealName()), '</a></li> <li>', logout_link();
 					} else {
 						echo login_link();
