fb70e42ffbf37e246f0d0cc147338f34a96b2d47
Jonathan Jaubart <dev@jaubart.com>
2015-08-29 17:40:28 +0100

    RESOLVED - #JJ195: Upgrade to webtrees 1.7.1 - Adapt Perso add-ons http://bugs.jaubart.com/view.php?id=195

---
+++ less/compile.cmd
@@ -0,0 +1,3 @@
+call lessc ./style.less ./../css-1.7.0/style-ltr.css  --verbose  --no-color --rtl="dir=LTR" --clean-css
+
+call lessc ./style.less ./../css-1.7.0/style-rtl.css  --verbose  --no-color --rtl="dir=RTL" --clean-css
--- theme.php
+++ theme.php
@@ -1,115 +1,266 @@
 <?php
-// Rural theme
-//
-// webtrees: Web based Family History software
-// Copyright (C) 2014 webtrees development team.
-//
-// Derived from PhpGedView
-// Copyright (C) 2002 to 2009  PGV Development Team.
-//
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 2 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
-//
-// PNG Icons By:Alessandro Rei; License: GPL; http://www.kde-look.org/content/show.php/Dark-Glass+reviewed?content=67902
-//
-// @author Jonathan Jaubart <dev@jaubart.com>
+/**
+ * webtrees-MyArtJaub
+ * Copyright (C) 2015 Jonathan Jaubart
+ * 
+ * Based on webtrees: online genealogy
+ * Copyright (C) 2015 webtrees development team
+ *  
+ * This file is part of webtrees-MyArtJaub
+ *  
+ * webtrees-MyArtJaub is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * webtrees-MyArtJaub is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with webtrees-MyArtJaub. If not, see <http://www.gnu.org/licenses/>.
+ */
 
-if (!defined('WT_WEBTREES')) {
-	header('HTTP/1.0 403 Forbidden');
-	exit;
+namespace MyArtJaub\Webtrees\Theme;
+
+use Fisharebest\Webtrees\I18N;
+use Fisharebest\Webtrees\FlashMessages;
+use Fisharebest\Webtrees\Menu;
+
+/**
+ * Class RuralTheme - Main class for Rural Theme.
+ */
+class RuralTheme extends \Fisharebest\Webtrees\Theme\AbstractTheme implements
+	\Fisharebest\Webtrees\Theme\ThemeInterface
+{
+	
+	/**
+	 * {@inheritdoc}
+	 */
+	public function themeId() {
+		return 'rural';
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function themeName() {
+		return /* I18N: Name of the Rural theme. */ I18N::translate('Rural');
+	}
+	
+	/** {@inheritdoc} */
+	public function assetUrl() {
+		return 'themes/rural/css-1.7.0/';
+	}
+	
+	/** {@inheritdoc} */
+	protected function favicon() {
+		return '<link rel="icon" href="' . $this->assetUrl() . 'favicon.png" type="image/png">';
+	}
+	
+	/** {@inheritdoc} */
+	protected function stylesheets() {		
+		$stylesheets = parent::stylesheets();
+		
+		$stylesheets[] = 'themes/rural/jquery-ui-1.11.2/jquery-ui.css';
+		$stylesheets[] = $this->assetUrl() . 'style-'.I18N::direction().'.css';
+		
+		return $stylesheets;
+	}
+	
+	/** {@inheritdoc} */
+	public function hookFooterExtraJavascript() {
+		return
+		'<script src="' . WT_BOOTSTRAP_JS_URL . '"></script>' .
+		'<script src="' . WT_JQUERY_COLORBOX_URL . '"></script>' .
+		'<script src="' . WT_JQUERY_WHEELZOOM_URL . '"></script>' .
+		'<script>
+		activate_colorbox();
+	
+		jQuery("body").on("click", "a.gallery", function(event) {
+			// Add colorbox to pdf-files
+			jQuery("a[type^=application].gallery").colorbox({
+				innerWidth: "75%",
+				innerHeight:"75%",
+				rel:        "gallery",
+				iframe:     true,
+				photo:      false,
+				slideshow:     true,
+				slideshowAuto: false,
+				title:		function(){
+					var url = jQuery(this).attr("href");
+					var img_title = jQuery(this).data("title");
+					return "<a href=\"" + url + "\" target=\"_blank\">" + img_title + "</a>";
+				}
+			});
+		});
+		jQuery.extend(jQuery.colorbox.settings, {
+			initialWidth: "20%", initialHeight: "20%",
+			slideshowStart: "<div id=\"cboxSlideshowStart\">&nbsp;</div>",
+			slideshowStop: "<div id=\"cboxSlideshowStop\">&nbsp;</div>",
+			transition: "none",
+			current: "{current} '.I18N::translate('of').' {total}",
+			title: function(){
+				var img_title = jQuery(this).data("title");
+				return img_title;
+			}
+		});
+		</script>';
+	}
+	
+	/** {@inheritdoc} */
+	public function parameter($parameter_name) {
+		$parameters = array(
+				'chart-background-f'             => 'e9daf1',
+				'chart-background-m'             => 'b1cff0',
+				'distribution-chart-high-values' => '9ca3d4',
+				'distribution-chart-low-values'  => 'e5e6ef'
+		);
+	
+		if (array_key_exists($parameter_name, $parameters)) {
+			return $parameters[$parameter_name];
+		} else {
+			return parent::parameter($parameter_name);
+		}
+	}	
+	
+	/** {@inheritdoc} */
+	public function bodyHeader() {
+		return
+		'<body id="body">' .
+			'<div class="row">'.
+				'<div class="main-col-layout">'.
+					'<header>' .
+						$this->headerContent() .
+					'</header>' .
+					'<div class="content_box">' .
+						$this->primaryMenuContainer($this->primaryMenu()) .
+						'<main id="content" role="main">' .
+						$this->flashMessagesContainer(FlashMessages::getMessages());
+	}
+	
+	
+	/** {@inheritdoc} */
+	public function bodyHeaderPopupWindow() {
+		return '<body id="body" class="popupwindow">' .
+			'<div class="row">'.
+				'<div class="main-col-layout">'.
+					'<div class="content_box">' .
+                        '<main id="content" role="main">' .
+                        $this->flashMessagesContainer(FlashMessages::getMessages());
+	}
+	
+	/** {@inheritdoc} */
+	public function footerContainer() {
+		return 
+					'</main>'.
+					'<footer>' .
+						'<div class="footer_left">'.
+							'<div class="footer_right">'.
+								'<div class="footer_center">'.
+								$this->footerContent() .
+								'</div>' . 	// -- .footer_center
+							'</div>' .	// -- .footer_right
+						'</div>' .	// -- .footer_left
+					'</footer>' .
+				'</div>'.	// -- #content_box
+			'</div>'. 	// -- .main-col-layout
+		'</div>';	// -- .row	
+	}
+	
+	/** {@inheritdoc} */
+	public function footerContainerPopupWindow() {
+		return        '</main>'.
+				'</div>'.	// -- #content_box
+			'</div>'. 	// -- .main-col-layout
+		'</div>';	// -- .row		
+		;
+	}
+
+	/** {@inheritdoc} */
+	protected function headerContent() {
+		$secondary_menu = $this->secondaryMenu();
+		$login_menu = array_filter(array(
+			$this->menuLogin(),
+			$this->menuLogout(),
+		));
+		
+		return 
+			'<div id="header-content-lrg">'.
+				'<div class="header-row"><div id="header-top">'.
+					'<div id="header-user-links" class="">'.
+						$this->secondaryMenuContainer($secondary_menu).
+					'</div>'.
+				'</div></div>'.
+				'<div class="header-row"><div id="header-middle">'.
+					'<ul>' . implode(' | ', array_map(function (Menu $menu) { return $menu->getMenuAsList(); }, $login_menu)) . '</ul>'.
+					//$this->secondaryMenuContainer($login_menu).
+					$this->formatTreeTitle() .
+				'</div></div>'.
+				'<div class="header-row"><div id="header-bottom">'.
+					$this->formQuickSearch().
+				'</div></div>'.
+			'</div>'.     // --  #header-content-lrg
+            '<div id="header-content-xs">'.
+                '<nav class="secondary-menu-nav-xs-holder">'.
+                    '<div class="secondary-menu-nav-xs-header">'.
+                        '<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#secondary-menu-nav-xs" aria-expanded="false">'.
+                            '<span class="icon-bar"></span>' .
+                            '<span class="icon-bar"></span>' .
+                            '<span class="icon-bar"></span>' .
+                        '</button>' .
+                        '<a class="navbar-brand secondary-menu-nav-xs-brand" href="#">'.$this->formatTreeTitle() .'</a>'.
+                    '</div>'.
+                    '<div id="secondary-menu-nav-xs">'.
+                        '<ul class="nav nav-pills secondary-menu">' . 
+                            $this->secondaryMenuContent(array_merge($login_menu, $secondary_menu)) . 
+                        '</ul>' .
+                    '</div>' .
+                '</nav>' .
+            '</div>'
+        ;
+	}
+	
+	/** {@inheritdoc} */
+	protected function secondaryMenu() {
+		return array_filter(array(
+				$this->menuPendingChanges(),
+				$this->menuMyPages(),
+				$this->menuFavorites(),
+				$this->menuThemes(),
+				$this->menuLanguages()
+		));
+	}
+	
+    /** {@inheritdoc} */
+    protected function primaryMenuContainer(array $menus) {
+		return '<nav class="primary-menu-nav-holder">'.
+            '<div class="primary-menu-nav-header">'.
+                '<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#primary-menu-nav" aria-expanded="false">'.
+                    '<span class="icon-bar"></span>' .
+                    '<span class="icon-bar"></span>' .
+                    '<span class="icon-bar"></span>' .
+                '</button>' .
+                '<a class="navbar-brand primary-menu-brand" href="#">'.I18N::translate('Menu').'</a>'.
+            '</div>'.
+            '<div id="primary-menu-nav">'.
+                '<ul class="primary-menu">' . 
+                    $this->primaryMenuContent($menus) . 
+                '</ul>' .
+            '</div>' .
+        '</nav>';
+	}
+    
+    /** {@inheritdoc} */
+	protected function primaryMenuContent(array $menus) {
+		return implode('', array_map(function (Menu $menu) { return $menu->bootstrap(); }, $menus));
+	}
+	
+    /** {@inheritdoc} */
+	protected function secondaryMenuContent(array $menus) {
+		return implode('', array_map(function (Menu $menu) { return $menu->bootstrap(); }, $menus));
+	}
+
+
 }
 
-// Theme name - this needs double quotes, as file is scanned/parsed by script
-$theme_name = "Rural";
-
-// A version number in the path prevents browser-cache problems during upgrade
-define('WT_CSS_URL', WT_THEME_URL . 'css-1.6.2/');
-
-$headerfile       = WT_THEME_DIR.'header.php';
-$footerfile       = WT_THEME_DIR.'footer.php';
-
-// Legacy icons.
-$WT_IMAGES=array(
-	'dline'			=>	WT_CSS_URL.'images/dline.png',
-	'dline2'		=>	WT_CSS_URL.'images/dline2.png',
-	'hline'			=>	WT_CSS_URL.'images/hline.png',
-	'spacer'		=>	WT_CSS_URL.'images/spacer.png',
-	'vline'			=>	WT_CSS_URL.'images/vline.png',
-
-	// used in button images and javascript
-	'add'            =>	WT_CSS_URL.'images/add.png',
-	'button_family'  =>	WT_CSS_URL.'images/buttons/family.png',
-	'minus'          =>	WT_CSS_URL.'images/minus.png',
-	'plus'           =>	WT_CSS_URL.'images/plus.png',
-	'remove'         =>	WT_CSS_URL.'images/remove.png',
-	'search'         =>	WT_CSS_URL.'images/search.png',
-
-	// need different sizes before moving to CSS
-	'default_image_M'=>	WT_CSS_URL.'images/silhouette_male.png',
-	'default_image_F'=>	WT_CSS_URL.'images/silhouette_female.png',
-	'default_image_U'=>	WT_CSS_URL.'images/silhouette_unknown.png',
-
-);
-
-//-- variables for the fan chart
-$fanChart = array(
-	'font'    =>WT_ROOT.'includes/fonts/DejaVuSans.ttf',
-	'size'    => 7,
-	'color'   =>'#000000',
-	'bgColor' =>'#eeeeee',
-	'bgMColor'=>'#b1cff0',
-	'bgFColor'=>'#e9daf1'
-);
-
-// Pedigree chart variables
-$bwidth			= 250;		// width of boxes on pedigree chart
-$bheight		= 80;		// height of boxes on pedigree chart
-$baseyoffset	= 10;		// position the entire pedigree tree relative to the top of the page
-$basexoffset	= 10;		// position the entire pedigree tree relative to the left of the page
-$bxspacing		= 1;		// horizontal spacing between boxes on the pedigree chart
-$byspacing		= 5;		// vertical spacing between boxes on the pedigree chart
-$linewidth		= 1.5;		// width of joining lines
-$shadowcolor	= '';		// shadow color for joining lines
-$shadowblur 	= 0;		// shadow blur for joining lines
-$shadowoffsetX	= 0;		// shadowOffsetX for joining lines
-$shadowoffsetY	= 0;		// shadowOffsetY for joining lines
-
-// Descendancy and relationship chart variables
-$Dbaseyoffset	= 20;		// position the entire descendancy tree relative to the top of the page
-$Dbasexoffset	= 20;		// position the entire descendancy tree relative to the left of the page
-$Dbxspacing		= 5;		// horizontal spacing between boxes
-$Dbyspacing		= 10;		// vertical spacing between boxes
-$Dbwidth		= 260;		// width of DIV layer boxes
-$Dbheight		= 80;		// height of DIV layer boxes
-$Dindent		= 15;		// width to indent descendancy boxes
-$Darrowwidth	= 30;		// additional width to include for the up arrows
-
-// Compact version of chart displays
-$cbwidth		= 240;
-$cbheight		= 50;
-
-// The largest possible area for charts is 300,000 pixels. As the maximum height or width is 1000 pixels
-$WT_STATS_S_CHART_X = 440;
-$WT_STATS_S_CHART_Y = 125;
-$WT_STATS_L_CHART_X = 900;
-
-// For map charts, the maximum size is 440 pixels wide by 220 pixels high
-$WT_STATS_MAP_X	= 440;
-$WT_STATS_MAP_Y	= 220;
-
-$WT_STATS_CHART_COLOR1="ffffff";
-$WT_STATS_CHART_COLOR2="9ca3d4";
-$WT_STATS_CHART_COLOR3="e5e6ef";
-
-?>
+return new RuralTheme();
