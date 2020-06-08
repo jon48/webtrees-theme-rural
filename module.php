e9001204087abf6b0b34752ba022c256472c3699
Jonathan Jaubart <dev@jaubart.com>
2020-06-08 10:14:38 +0100

    RESOLVED #9 - Sources are expanded by default

--- module.php
+++ module.php
@@ -128,7 +128,7 @@ class RuralTheme extends MinimalTheme implements ModuleCustomInterface, ModuleFo
      */
     public function customModuleVersion(): string
     {
-        return '2.0.4-v.2';
+        return '2.0.4-v.3';
     }
 
     /**
