8ba1b386ce8f43c07ceedb269e40c07bb1613272
Jonathan Jaubart <dev@jaubart.com>
2022-05-01 11:05:30 +0100

    RESOLVED #JJ289: Upgrade to webtrees 2.1.1 - Rural theme upgrade https://bugs.jaubart.com/view.php?id=289

--- module.php
+++ module.php
@@ -107,7 +107,7 @@ class RuralTheme extends AbstractModule implements ModuleCustomInterface, Module
      */
     public function customModuleVersion(): string
     {
-        return '2.1.0-v.1';
+        return '2.1.1-v.1';
     }
 
     /**
